4724f19c906bfb6aa56a3f0680fb001e69c041d3
scleng14 <chelengsoo14@1utar.my>
2025-07-09 00:24:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import cv2
 import numpy as np
-from PIL import Image, ExifTags
+from PIL import Image, ImageOps
 import pandas as pd
 from datetime import datetime
 import os
