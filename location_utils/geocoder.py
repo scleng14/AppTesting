85ca4ca85106315d256f97355c7facdeb63a5fab
scleng14 <chelengsoo14@1utar.my>
2025-07-09 00:27:01 +0800

    Update geocoder.py

--- location_utils/geocoder.py
+++ location_utils/geocoder.py
@@ -1,13 +1,13 @@
+# geocoder.py
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
-geolocator = Nominatim(user_agent="geo_locator_v1")
-reverse_geocode = RateLimiter(geolocator.reverse, min_delay_seconds=1)
+_geolocator = Nominatim(user_agent="location_detector")
+_reverse = RateLimiter(_geolocator.reverse, min_delay_seconds=1)
 
-def get_address_from_coords(coords):
-    """坐标反查地址"""
+def reverse_geocode(coords, language='en'):
     try:
-        location = reverse_geocode(coords, language='en')
+        location = _reverse(coords, language=language)
         return location.address if location else None
     except Exception:
         return None
