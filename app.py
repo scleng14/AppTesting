d2ed368143d78af21a74769947f92d5d4baa8cbe
scleng14 <chelengsoo14@1utar.my>
2025-07-12 17:30:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -479,12 +479,10 @@ def main_app():
             st.markdown("<hr style='width: 325px; margin-top: 0;'>", unsafe_allow_html=True)
             
             coords_result = st.session_state.get("coords_result", None)
-            method = st.session_state.get("location_method", "")
+            method = st.session_state.get("location_method", "Unknown")
             landmark = st.session_state.get("landmark", "N/A")
             
-            # Initialize location variable
-            location = "Unknown"
-            if coords_result:
+            if coords_result and location != "Unknown":
                 # Get location from coordinates if available
                 location = get_address_from_coords(coords_result)
             
