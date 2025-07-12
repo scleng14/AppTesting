3d9b63c5b20bcdc4d1465c22994fd1bd4bb7b218
scleng14 <chelengsoo14@1utar.my>
2025-07-12 13:27:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -467,9 +467,9 @@ def main_app():
 
         with tabs[1]:
             st.subheader("🗺️ Detected Location Map")
-            #st.markdown("""<hr style='width: fit-content; border: 1px solid #999; margin-top: 0;'>""", unsafe_allow_html=True)
-            st.markdown("<hr style='width: 80%; margin-left: auto; margin-right: auto;'>", unsafe_allow_html=True)
-            #st.markdown("<hr style='width: fit-content; margin-top: 0; border: 1px solid #ccc;'>", unsafe_allow_html=True)
+            st.markdown("""<hr style='width: 30% fit-content; border: 1px solid #999; margin-top: 0;'>""", unsafe_allow_html=True)
+            #st.markdown("<hr style='width: 80%; margin-left: auto; margin-right: auto;'>", unsafe_allow_html=True)
+            st.markdown("<hr style='width: fit-content; margin-top: 0; border: 1px solid #ccc;'>", unsafe_allow_html=True)
             
             coords_result = st.session_state.get("coords_result", None)
             method = st.session_state.get("location_method", "")
